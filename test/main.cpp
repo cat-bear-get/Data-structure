98dd96847ee977ee15d2f47447309579c9c3de72
cat-bear-get <2416078929@qq.com>
2022-06-20 13:33:01 +0800

    测试主函数

--- test/main.cpp
+++ test/main.cpp
@@ -2,7 +2,6 @@
 #include "SqList.h"
 #include "HomeWork.h"
 
-
 /*********************************
  * 顺序表测试
  */
@@ -22,10 +21,10 @@ void TestSqList()
     int del_min_value = 0;
     Del_Min(L, del_min_value);
     printf("\tDelValue=%d\n", del_min_value);
-    PrintSqList(L,"删除最小元素后的顺序表");
+    PrintSqList(L, "删除最小元素后的顺序表");
 
     Reverse(L);
-    PrintSqList(L,"逆序排列后的顺序表");
+    PrintSqList(L, "逆序排列后的顺序表");
 
     InitSqList(L);
     printf("顺序表\n");
@@ -33,51 +32,71 @@ void TestSqList()
 
     int del_value = 10;
     Del_x_1(L, del_value);
-    PrintSqList(L,"删除元素10后的顺序表");
+    PrintSqList(L, "删除元素10后的顺序表");
 
     int s2 = 3, t2 = 7;
     Del_s_t2(L, s2, t2);
-    PrintSqList(L,"删除3~~7后的顺序表");
+    PrintSqList(L, "删除3~~7后的顺序表");
 
     InitSqList(L);
     PutIntoSqList(L, value2, 15);
     int s = 3, t = 7;
     Del_s_t(L, s, t);
-    PrintSqList(L,"删除3~~7后的顺序表");
+    PrintSqList(L, "删除3~~7后的顺序表");
 
     int value3[11] = {1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 5};
     InitSqList(L);
     printf("顺序表\n");
     PutIntoSqList(L, value3, 11);
     Delete_Same(L);
-    PrintSqList(L,"删除相同元素后顺序表");
+    PrintSqList(L, "删除相同元素后顺序表");
 
-    int value4[4]={1,2,4,5},value5[4]={1,3,8,9};
-    SqList A,B,C;
-    InitSqList(A);InitSqList(B);InitSqList(C);
-    PutIntoSqList(A,value4,4);PutIntoSqList(B,value5,4);
-    PrintSqList(A,"顺序表(A)");PrintSqList(B,"顺序表(B)");
-    Merge(A,B,C);
-    PrintSqList(C,"合并后的顺序表为");
+    int value4[4] = {1, 2, 4, 5}, value5[4] = {1, 3, 8, 9};
+    SqList A, B, C;
+    InitSqList(A);
+    InitSqList(B);
+    InitSqList(C);
+    PutIntoSqList(A, value4, 4);
+    PutIntoSqList(B, value5, 4);
+    PrintSqList(A, "顺序表(A)");
+    PrintSqList(B, "顺序表(B)");
+    Merge(A, B, C);
+    PrintSqList(C, "合并后的顺序表为");
 
-    int value6[8]={1,2,3,4,5,6,7,8};
-    Exchange(value6,4,4,8);
-    printf("交换后的数组\n");
+    int value6[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    Exchange(value6, 4, 4, 8);
+    printf("课后练习第8题\n");
     for (int i = 0; i < 8; i++)
     {
-        printf("value6[%d]=%d\n",i,value6[i]);
+        printf("value6[%d]=%d\n", i, value6[i]);
     }
     printf("#################################\n");
 
-    int value7[]={1,2,3,4,5,6,7,9,10};
-    //SearchExchangeInsert(value7,9,8);
-    printf("查找后的数组\n");
-    for (int i = 0; i < 9; i++)
+    int value7[] = {1, 2, 3, 4, 5, 6, 7, 9, 10};
+    SearchExchangeInsert(value7, 9, 8);
+    printf("课后练习第9题\n");
+    for (int i = 0; i < (int)(sizeof(value7) / sizeof(int)); i++)
     {
-        printf("value7[%d]=%d\n",i,value7[i]);
+        printf("value7[%d]=%d\n", i, value7[i]);
     }
     printf("#################################\n");
+
+    int value8[] = {1, 2, 3, 4, 5, 6, 7, 9, 10};
+    Converse(value8, sizeof(value8) / sizeof(int), 4);
+    printf("课后练习第10题\n");
+    for (int i = 0; i < (int)(sizeof(value7) / sizeof(int)); i++)
+    {
+        printf("%d  ", value8[i]);
+    }
+    printf("\n#################################\n");
+
+    int S1[] = {11, 13, 15, 17, 19}, S2[] = {2, 4, 5, 8, 20};
+    int mid = M_Search(S1, S2, 5);
+    printf("课后练习第11题\n");
+    printf("S1与S2的中位数为:%d\n", mid);
+    printf("#################################\n");
 }
+
 int main()
 {
     TestSqList();
